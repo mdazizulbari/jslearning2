f9486cc19e47589f4f50e6efdc732009eee9cfbc
SMA Bari <mdazizulbarishafin@gmail.com>
2024-07-13 10:04:45 +0600

    if statements js

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ let age = 110;
 if(age >= 100){
     console.log(`Go and spend time with Nature, use the time you have in something memorable.`);
 }
-else if(){
-    
+else if(age == 0){
+    console.log(`Don't start lying here, you are being tracked!`)
+}
+else if(age >= 18){
+    console.log(`Welcome!`)
 }
 else{
     console.log(`You shouldn't be here!`)
