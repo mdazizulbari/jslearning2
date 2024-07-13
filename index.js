050604b4387dd294fbe662b4bb888f36e6b7804d
SMA Bari <mdazizulbarishafin@gmail.com>
2024-07-13 09:59:40 +0600

    structure and some js

---
+++ index.js
@@ -0,0 +1,11 @@
+// if statements:
+let age = 110;
+if(age >= 100){
+    console.log(`Go and spend time with Nature, use the time you have in something memorable.`);
+}
+else if(){
+    
+}
+else{
+    console.log(`You shouldn't be here!`)
+}
