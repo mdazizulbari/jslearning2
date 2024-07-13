026ad6230ffd9b758c7c430cefd7688e58988141
SMA Bari <mdazizulbarishafin@gmail.com>
2024-07-13 10:40:49 +0600

    made it dynamic

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 // if statements:
-let age = 110;
-if(age >= 100){
-    console.log(`Go and spend time with Nature, use the time you have in something memorable.`);
-}
-else if(age == 0){
-    console.log(`Don't start lying here, you are being tracked!`)
-}
-else if(age >= 18){
-    console.log(`Welcome!`)
-}
-else{
-    console.log(`You shouldn't be here!`)
+const ifageinput = document.getElementById('ifageinput');
+const ifagebutton = document.getElementById('ifagebutton');
+const resultif = document.getElementById('resulfif');
+let age;
+ifagebutton.onclick = function(){
+    age = ifageinput.value;
+    age = Number(age);
+    if(age >= 100){
+        resultif.textContent = `Go and spend time with Nature, use the time you have in something memorable.`;
+    }
+    else if(age == 0, age <= 0){
+        resultif.textContent = `Don't start lying here, you are being tracked!`;
+    }
+    else if(age >= 18){
+        resultif.textContent = `Welcome!`;
+    }
+    else if(age = ''){
+        resultif.textContent = `Told you to enter the age first.`
+    }
+    else{
+        resultif.textContent = `You shouldn't be here! Become 18 first.`;
+    }
 }
